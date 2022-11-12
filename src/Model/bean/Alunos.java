22dd09ea4b3dae4b2e7165058c4086a834e3001c
Juan Pedro <jpss332211@gmail.com>
2022-11-12 00:25:40 -0300

    Att classe Alunos

---
+++ src/Model/bean/Alunos.java
@@ -0,0 +1,16 @@
+package Model.bean;
+// @author jpss3
+ 
+public class Alunos extends Pessoas{
+    
+    private float mensalidade;
+    private float multaatraso;       
+    
+    public void Alunos(float mensalidade, float multaatraso){
+        
+        super(String nome,String cpf,String endereco,String telefone,int idade);
+        
+        
+    }
+  
+}
